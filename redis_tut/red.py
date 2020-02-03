086cee5fb76cde9eec4b9dc5f6b7aba6674c6217
free4m <chgbarnes@gmail.com>
2020-02-03 13:44:06 -0600

    redis_tut/

---
+++ redis_tut/red.py
@@ -0,0 +1,63 @@
+import random
+import logging
+import redis
+
+
+random.seed(444)
+hats = {f"hat:{random.getrandbits(32)}": i for i in (
+    {
+        "color": "black",
+        "price": 49.99,
+        "style": "fitted",
+        "quantity": 1000,
+        "npurchased": 0,
+    },
+    {
+        "color": "maroon",
+        "price": 59.99,
+        "style": "hipster",
+        "quantity": 500,
+        "npurchased": 0,
+    },
+    {
+        "color": "green",
+        "price": 99.99,
+        "style": "baseball",
+        "quantity": 200,
+        "npurchased": 0,
+    })
+}
+
+
+logging.basicConfig()
+
+
+class OutOfStockError(Exception):
+    """Raised when PyHats.com is all out of today's hottest hat"""
+
+
+def buyitem(r: redis.Redis, itemid: int) -> None:
+    with r.pipeline() as pipe:
+        error_count = 0
+        while True:
+            try:
+                # Get available inventory, watching for changes
+                # related to this itemid before the transaction
+                pipe.watch(itemid)
+                nleft: bytes = r.hget(itemid, "quantity")
+                if nleft > b"0":
+                    pipe.multi()
+                    pipe.hincrby(itemid, "quantity", -1)
+                    pipe.hincrby(itemid, "npurchased", 1)
+                    pipe.execute()
+                    break
+                else:
+                    # Stop watching the itemid and raise to break out
+                    pipe.unwatch()
+                    raise OutOfStockError(f"Sorry, {itemid} is out of stock!")
+            except redis.WatchError:
+                # Stop watching the itemid and raise to break out
+                error_count += 1
+                logging.warning("WatchError #%d: %s; retrying", error_count, itemid)
+
+    return None
